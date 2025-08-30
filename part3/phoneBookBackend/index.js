3e205c291a085b9049bc4fe57f427886199ef816
ashishkumarchandan <aaassshhhiiissshhh111@gmail.com>
2025-08-30 12:08:41 +0000

    ads

--- part3/phoneBookBackend/index.js
+++ part3/phoneBookBackend/index.js
@@ -18,8 +18,8 @@ let notes = [
     important: true,
   },
   {
-    id: "3",
-    content: "uwu boobs",
+    id: "4",
+    content: "cyberpunk",
     important: true,
   },
 ];
@@ -31,6 +31,67 @@ app.get("/", (request, response) => {
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const note = notes.find((note) => {
+    return note.id === id;
+  });
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).send(`<h1>nah its not there </h1>`);
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedNotes = notes.filter((note) => {
+    return note.id !== id;
+  });
+  notes = updatedNotes;
+  res.status(204).send("Deletion Completed");
+});
+
+const generateId = () => {
+  let maxId = 0;
+  if (notes.length > 0) {
+    maxId = Math.max(
+      ...notes.map((n) => {
+        return Number(n.id);
+      })
+    );
+  }
+  return String(maxId + 1);
+};
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    res.status(400).json({
+      error: "content missing",
+    });
+
+    return;
+  }
+
+  const note = {
+    ...body,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes.push(note);
+
+  res.json(note);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
